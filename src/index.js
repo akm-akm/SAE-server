75b2540c5fc820f6dd77e98e333f0b262a483ca4
akm-akm <adityanick08@gmail.com>
2022-01-08 23:17:36 +0530

    .

--- src/index.js
+++ src/index.js
@@ -1,26 +1,28 @@
-const cors = require('cors')
-const express = require('express')
-const app = new express()
-const path = require('path')
-require('dotenv').config()
-require(path.join(__dirname, './model/mongoose'))
-const messagesroute = require(path.join(__dirname, './routes/messages'))
-const blogroute = require(path.join(__dirname, './routes/blog'))
-const alumniroute = require(path.join(__dirname, './routes/alumni'))
-const post_bearerroute = require(path.join(__dirname, './routes/post_bearer'))
-const signuproute = require(path.join(__dirname, './routes/signup'))
-const loginroute = require(path.join(__dirname, './routes/login'))
-const logoutroute = require(path.join(__dirname, './routes/logout'))
+const cors = require("cors");
+const express = require("express");
+const app = new express();
+const path = require("path");
+require("dotenv").config();
+const port = process.env.PORT || 5005;
+require(path.join(__dirname, "./model/mongoose"));
+const messagesroute = require(path.join(__dirname, "./routes/messages"));
+const blogroute = require(path.join(__dirname, "./routes/blog"));
+const alumniroute = require(path.join(__dirname, "./routes/alumni"));
+const post_bearerroute = require(path.join(__dirname, "./routes/post_bearer"));
+const signuproute = require(path.join(__dirname, "./routes/signup"));
+const loginroute = require(path.join(__dirname, "./routes/login"));
+const logoutroute = require(path.join(__dirname, "./routes/logout"));
 
-app.use(express.static(path.join(__dirname, '../client/build/')))
-    .use(express.json())
-    .use(cors())
-    .use('/api', messagesroute)
-    .use('/api', blogroute)
-    .use('/api', alumniroute)
-    .use('/api', post_bearerroute)
-    .use('/api', signuproute)
-    .use('/api', loginroute)
-    .use('/api', logoutroute)
-    .get('*', (req, res) => res.status(200).sendFile('index.html'))
-    .listen(5005, () => console.log('http://localhost:5005'))
+app
+  .use(express.static(path.join(__dirname, "../client/build/")))
+  .use(express.json())
+  .use(cors())
+  .use("/api", messagesroute)
+  .use("/api", blogroute)
+  .use("/api", alumniroute)
+  .use("/api", post_bearerroute)
+  .use("/api", signuproute)
+  .use("/api", loginroute)
+  .use("/api", logoutroute)
+  .get("*", (_, res) => res.status(200).sendFile("index.html"))
+  .listen(port, () => console.log("http://localhost:" + port));
--- src/model/mongoose.js
+++ src/model/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/sae_db2', {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
    
